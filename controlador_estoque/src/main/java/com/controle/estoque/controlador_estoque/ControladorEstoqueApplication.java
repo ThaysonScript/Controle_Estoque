77fb0592018aac7fcf1538c737c6a7365760e9bd
ThaysonScript <thaysonguedes01@gmail.com>
2024-03-02 20:53:46 -0300

    base

---
+++ controlador_estoque/src/main/java/com/controle/estoque/controlador_estoque/ControladorEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.controle.estoque.controlador_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorEstoqueApplication.class, args);
+	}
+
+}
---
+++ controlador_estoque/src/test/java/com/controle/estoque/controlador_estoque/ControladorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controle.estoque.controlador_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
